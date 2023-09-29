57068e029a8fbc8a3eccdf74c686e4546c26a7c1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 09:13:34 -0300

    finalizando o checkpoint

---
+++ script.js
@@ -0,0 +1,24 @@
+let nome = "Gabriel";
+let idade = 19; 
+let status = true;
+let valorProduto = 10.50;
+let listaTimes = ["Corinthians", "Racing", "Inter Miami"]
+
+console.log("Meu nome é:" , nome);
+console.log("Minha idade é" , idade);
+console.log("A variavél é" , status)
+console.log("O valor do produto é de" , valorProduto);
+console.log("Os meus times são:", listaTimes);
+
+let num1 = 2;
+let num2 = 8;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log("O resultado da soma é:", soma);
+console.log("O resultado da subtração é" , subtracao);
+console.log("O resultado da multiplicação é:", multiplicacao);
+console.log("O resultado da divisão é:" , divisao);
